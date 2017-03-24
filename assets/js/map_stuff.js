431a84c74495987a2cdfe2356127ac052353ae00
Ege Demirel <egedemirel@gmail.com>
2017-03-23 23:24:30 -0500

    update osm update

--- assets/js/map_stuff.js
+++ assets/js/map_stuff.js
@@ -2,10 +2,10 @@ var gen_map = L.map('general_map').setView([38.39172, 27.08473], 10);
 var izm_map = L.map('izmir_map').setView([38.30018, 27.15285], 12);
 
 
-L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(gen_map);
-L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(izm_map);
 // L.tileLayer('https://a.tiles.mapbox.com/v3/mapbox.world-bright/{z}/{x}/{y}.png').addTo(map);
